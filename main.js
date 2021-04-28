1b058fecb68f356717632b13f7831d1e5abf05bc
alay_1884 <alaychetankumar.parikh@mavs.uta.edu>
2021-04-28 13:23:34 -0700

    function main() {

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 function main() {
   console.log("Hello world");
+  var num = 21;
+  
+  print(num); 
 }
+
+function print(num){
+	var arr = ["zero","one", "two","three","four","five","six","seven","eight","nine"];
+	var str = "";
+	if(num == 0){
+  	console.log(arr[0])
+  	return;
+  }
+	while(num >0){
+		rem = num % 10;
+		num = Math.floor (num / 10);
+		str = arr[rem] + str;
+	}
+	console.log(str);
+
+}
+
 
 
 if (require.main == module) {
