7620e036f45cf97ddbdfd6381b0bb62dd85fc241
alay_1884 <alaychetankumar.parikh@mavs.uta.edu>
2021-04-28 21:32:20 -0700

    Fully functional: Converting array of integers to string

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 function main() {
-  console.log("Hello world");
-  var num = 21;
-  
-  print(num); 
+
+	// Null check validation
+	if (process.argv.length == 2){ 
+		console.log("Please enter valid numbers");
+	}
+	var args = process.argv.slice(2);
+	var result;
+
+
+	for (var i = 0; i <= args.length - 1 ; i++) {
+		currStr = printNumToString(args[i]);
+		if(i == 0)
+			result = currStr;
+		else
+			result = result + "," + currStr;
+	}
+  console.log(result);
 }
 
-function print(num){
-	var arr = ["zero","one", "two","three","four","five","six","seven","eight","nine"];
+/**
+* Creates new function to print Number to String
+* param: Number (Integers)
+* return Converted String
+**/
+
+function printNumToString(num){
+	var arr = ["Zero","One", "Two","Three","Four","Five","Six","Seven","Eight","Nine"];
 	var str = "";
 	if(num == 0){
-  	console.log(arr[0])
-  	return;
+  	return arr[0];
   }
 	while(num >0){
 		rem = num % 10;
 		num = Math.floor (num / 10);
 		str = arr[rem] + str;
 	}
-	console.log(str);
-	console.log("hi");
+	return str;
 }
 
 
-
-if (require.main == module) {
+if (require.main === module) {
   main();
 }
